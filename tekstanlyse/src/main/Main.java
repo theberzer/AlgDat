e2423d714797608368b784b69e0011c527ca26fa
Unknown <berzi.w@gmail.com>
2017-04-16 14:53:38 +0200

    Uploaded Tekstanalyse WIP

---
+++ tekstanlyse/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+
+
+	}
+
+}
